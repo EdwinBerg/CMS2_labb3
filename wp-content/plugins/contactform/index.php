122c1542154737ee3558d33cb0faa5c7c6f2bba4
Edwin Berg <e.berg117@gmail.com>
2020-11-24 21:09:32 +0100

    fixed class, done

--- wp-content/plugins/contactform/index.php
+++ wp-content/plugins/contactform/index.php
@@ -3,7 +3,20 @@
   Plugin Name: Kontaktformulär
   Description: Detta plugin ska kunna skriva ut ett kontaktformulär!
 */
-function contact_form() { ?>
+
+class Kontakt_Formular {
+
+public function __construct() {
+    echo 'hej';
+    add_action('wp_ajax_send_contact_form', array($this, 'recieve_contact_form'));
+    add_action('init', array($this,'messages'));
+    add_action('send_contact_form', array($this, 'insertpost_from_contact_form'));
+    do_action('save_post_{$post->messages}');
+    add_action('woocommerce_before_main_content', array($this, 'contact_form')); 
+  }
+
+
+public function contact_form() { ?>
   <div>
     <form action="<?php echo admin_url('admin-ajax.php');?>">
       <input type="text" name="name" placeholder="Skriv in namn..">
@@ -17,7 +30,7 @@ function contact_form() { ?>
 <?php 
 }
 
-function recieve_contact_form(){
+public function recieve_contact_form(){
   echo "<p> Tack så mycket " . $_REQUEST['name'] . "!</p>" ;
   wp_insert_post(
     [
@@ -30,24 +43,18 @@ function recieve_contact_form(){
   die();
 }
 
-add_action('wp_ajax_send_contact_form', 'recieve_contact_form');
-
-
-function messages() {
-  register_post_type('meddelanden', [
-    'labels' => [
-      'name' => 'Meddelanden',
-      'singular_name' => 'Meddelande'
-    ],
-    'public' => true,
-    'has_archive' => true
-   ]);
+  public function messages() {
+    register_post_type('meddelanden', [
+      'labels' => [
+        'name' => 'Meddelanden',
+        'singular_name' => 'Meddelande'
+      ],
+      'public' => true,
+      'has_archive' => true
+      ]);
+    }
 }
-
-
-
-add_action('init', 'messages');
-add_action('send_contact_form', 'insertpost_from_contact_form');
-do_action('save_post_{$post->messages}');
-add_action('woocommerce_before_main_content', 'contact_form');
+if(class_exists('Kontakt_Formular')) {
+  $Kontakt_Formular = New Kontakt_Formular();
+}
 ?>
