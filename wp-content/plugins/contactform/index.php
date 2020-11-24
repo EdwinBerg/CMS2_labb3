f50e006d93e9e7a61367cc0ecaf25f6c29371706
Edwin Berg <e.berg117@gmail.com>
2020-11-24 19:26:46 +0100

    fixade så man kan skriva in sitt namn och de sparas i db och i meddelanden på wp

---
+++ wp-content/plugins/contactform/index.php
@@ -0,0 +1,68 @@
+<?php 
+/*
+  Plugin Name: Kontaktformulär
+  Description: Detta plugin ska kunna skriva ut ett kontaktformulär!
+*/
+function contact_form() { ?>
+  <div>
+    <form action="<?php echo admin_url('admin-ajax.php');?>">
+      <input type="text" name="name" placeholder="Skriv in namn..">
+      <br>
+      <input type="textarea" name="message" placeholder="Skriv ditt meddelande..">
+      <br>
+      <input type="submit" name="submit" value="Skicka">
+      <input type="hidden" name="action" value="send_contact_form">
+    </form>
+  </div>
+<?php 
+}
+
+function recieve_contact_form(){
+  echo "<p> Tack så mycket " . $_REQUEST['name'] . "!</p>" ;
+  wp_insert_post(
+    [
+      'post_title' => $_REQUEST['name'], 
+      'post_content' => $_REQUEST['message'],
+      'post_type' => 'meddelanden',
+      
+    ]
+    );
+  die();
+}
+
+add_action('wp_ajax_send_contact_form', 'recieve_contact_form');
+
+// function insertpost_from_contact_form() {
+//   wp_insert_post(
+//     [
+//       'post_title' => $_REQUEST['name'], 
+//       'post_content' => $_REQUEST['message'],
+//       'post_type' => 'meddelanden',
+      
+//     ]
+//     );
+// }
+
+
+function messages() {
+  register_post_type('meddelanden', [
+    'labels' => [
+      'name' => 'Meddelanden',
+      'singular_name' => 'Meddelande'
+    ],
+    'public' => true,
+    'has_archive' => true
+   ]);
+}
+
+
+
+add_action('init', 'messages');
+add_action('send_contact_form', 'insertpost_from_contact_form');
+do_action('save_post_{$post->messages}');
+add_action('woocommerce_before_main_content', 'contact_form');
+
+//wp_insertpost returnerar?
+// add_post_meta eller update_post_meta
+
+?>
