80e3c3ed0b1650368922cdfdb8d3077cf427b800
Edwin Berg <e.berg117@gmail.com>
2020-11-24 22:09:26 +0100

    fixed class, done

--- wp-content/plugins/count/index.php
+++ wp-content/plugins/count/index.php
@@ -3,34 +3,30 @@
   Plugin Name: Count my string
   Description: Count how many letter there is in a string, returning a true or false statement depending if the string is longer than seven letters.
 */
-?>
+class Seven_Letters_Long {
+  public $is_seven_letters_long;
+ 
+  
+  public function __construct(){
+    if(isset($_POST["count_letters"])) {
+    $this->is_seven_letters_long = filter_var($_POST["count_letters"], FILTER_SANITIZE_STRING);
 
-<?php 
-$is_seven_letters_long = '';
-if(isset($_POST["count_letters"])) {
-  $is_seven_letters_long = filter_var($_POST["count_letters"], FILTER_SANITIZE_STRING);
-
+    echo "<form action=\"\" method=\"post\">
+            <label>Skriv in ett ord: </label><br>
+            <input type=\"text\" name=\"count_letters\">
+          </form>";
+      
+     echo "<p> Ordet \"$this->is_seven_letters_long\" har " . strlen($this->is_seven_letters_long). "st bokstäver." . $this->counter($this->is_seven_letters_long) . "</p>";
+    }
+  }
+  public function counter($is_seven_letters_long) {
+    if(strlen($this->is_seven_letters_long) == 7 ) {
+      echo "<p>True</p>";
+    } else echo "<p>False</p>";    
+  }
+  
 }
-function counter($is_seven_letters_long) {
-  if(strlen($is_seven_letters_long) == 7 ) {
-     echo "<p>True</p>";
-  } else echo "<p>False</p>"; 
-}
+if(class_exists('Seven_Letters_Long') {
+  $seven_letters_long = New Seven_Letters_Long
+})
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <form action="" method="post">
-    <label>Skriv in ett ord: </label><br>
-    <input type="text" name="count_letters">
-  </form>
-  <?php echo "<p> Ordet \"$is_seven_letters_long\" har " . strlen($is_seven_letters_long). "st bokstäver." . counter($is_seven_letters_long); ?> 
-</body>
-</html>
-
